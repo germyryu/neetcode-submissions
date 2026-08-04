b628024cd648daf8856c01d426f0040e8b7511ab
Jeremy Ryu <60302455+germyryu@users.noreply.github.com>
2026-08-04 18:52:07 -0400

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,27 @@
+# [5, 0, 2]
+# 0, 0, 0
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.prefix_stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            min_val = val
+        else:
+            min_val = min(val, self.prefix_stack[-1])
+        self.stack.append(val)
+        self.prefix_stack.append(min_val)
+        print(self.prefix_stack)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.prefix_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.prefix_stack[-1]
+        
