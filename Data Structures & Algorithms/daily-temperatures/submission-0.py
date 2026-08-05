a5d588a025c678c0f0a82cc6c089306472c6e113
Jeremy Ryu <60302455+germyryu@users.noreply.github.com>
2026-08-05 08:22:52 -0400

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,26 @@
+'''
+keep a stack
+because length is always greater than or equal to 1, always add the first element to the stack
+
+for each element after that
+if it is smaller than the top of the stack, then push to the stack
+if it is larger than the top of the stack, then keep popping stack until top element of the stack is larger than the current element
+as we pop, keep track of the difference in indices and then push that difference to the result[]
+'''
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        result = [0] * len(temperatures)
+
+        for (i,v) in enumerate(temperatures):
+            print(stack)
+            if not stack:
+                stack.append((i,v))
+                continue
+            while stack and stack[-1][1] < temperatures[i]:
+                print(stack)
+                idx = stack[-1][0]
+                stack.pop()
+                result[idx] = i-idx
+            stack.append((i,v))
+        return result
